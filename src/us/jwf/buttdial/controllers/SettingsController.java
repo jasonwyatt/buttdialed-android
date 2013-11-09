430f0ba0b0b21f84923e312d96c655c52de1ec54
Jason Feinstein <jason.feinstein@gmail.com>
2013-11-09 14:05:20 -0500

    Settings work. Improved the intent generation for Google Voice somewhat - still cannot populate message field because its a bug.

--- src/us/jwf/buttdial/controllers/SettingsController.java
+++ src/us/jwf/buttdial/controllers/SettingsController.java
@@ -3,20 +3,81 @@ package us.jwf.buttdial.controllers;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import us.jwf.buttdial.App;
+import us.jwf.buttdial.R;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  */
 public class SettingsController {
     public static String PREF_USE_DEFAULT_SMS = "use_default_sms_app";
+    public static String PREF_MESSAGE_OPTIONS = "message_options";
+    public static String PREF_DEFAULT_MESSAGE = "default_message";
+
     private final App app;
+    private SharedPreferences prefs;
 
     public SettingsController(App app) {
         this.app = app;
+        prefs = PreferenceManager.getDefaultSharedPreferences(app);
     }
 
     public boolean useDefaultSMS() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(app);
         return prefs.getBoolean(PREF_USE_DEFAULT_SMS, true);
     }
+
+    public void setUseDefaultSMS(boolean useDefault) {
+        prefs.edit().putBoolean(PREF_USE_DEFAULT_SMS, useDefault).commit();
+    }
+
+    public Set<String> getMessageOptions() {
+        return prefs.getStringSet(PREF_MESSAGE_OPTIONS, getDefaultMessageOptionsSet());
+    }
+
+    public String[] getMessageOptionsArray() {
+        Set<String> options = getMessageOptions();
+        return options.toArray(new String[options.size()]);
+    }
+
+    public void setMessageOptions(String[] messageOptions) {
+        HashSet<String> optionSet = new HashSet<String>(messageOptions.length);
+        for (String message : messageOptions) {
+            optionSet.add(message);
+        }
+        setMessageOptions(optionSet);
+    }
+
+    public void setMessageOptions(Set<String> messageOptions) {
+        prefs.edit().putStringSet(PREF_USE_DEFAULT_SMS, messageOptions).commit();
+    }
+
+    public void addMessageOption(String message) {
+        Set<String> messageOptions = getMessageOptions();
+        messageOptions.add(message);
+        setMessageOptions(messageOptions);
+    }
+
+    public void removeMessageOption(String message) {
+        Set<String> options = getMessageOptions();
+        options.remove(message);
+        setMessageOptions(options);
+    }
+
+    public String getDefaultMessage() {
+        return prefs.getString(PREF_DEFAULT_MESSAGE, app.getString(R.string.apology_content));
+    }
+
+    public void setDefaultMessage(String message) {
+        prefs.edit().putString(PREF_DEFAULT_MESSAGE, message).commit();
+    }
+
+    private Set<String> getDefaultMessageOptionsSet() {
+        HashSet<String> defaultOptions = new HashSet<String>();
+        defaultOptions.add(app.getString(R.string.apology_content));
+        return defaultOptions;
+    }
+
 }
--- src/us/jwf/buttdial/views/SettingsFragment.java
+++ src/us/jwf/buttdial/views/SettingsFragment.java
@@ -3,9 +3,11 @@ package us.jwf.buttdial.views;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import us.jwf.buttdial.App;
 import us.jwf.buttdial.R;
 import us.jwf.buttdial.controllers.SettingsController;
 
@@ -13,16 +15,25 @@ import us.jwf.buttdial.controllers.SettingsController;
  *
  */
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private ListPreference defaultMessage;
+    private App app;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        app = (App) getActivity().getApplicationContext();
+
+        defaultMessage = (ListPreference)findPreference(SettingsController.PREF_DEFAULT_MESSAGE);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+        updatePrefs();
     }
 
     @Override
@@ -31,14 +42,19 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 
+    protected void updatePrefs() {
+        defaultMessage.setEntries(app.settings().getMessageOptionsArray());
+        defaultMessage.setEntryValues(app.settings().getMessageOptionsArray());
+        defaultMessage.setSummary("\""+ app.settings().getDefaultMessage() + "\"");
+        defaultMessage.setValue(app.settings().getDefaultMessage());
+    }
+
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(
-                getActivity().getApplicationContext()
-        ).edit();
         if (key.equals(SettingsController.PREF_USE_DEFAULT_SMS)){
-            editor.putBoolean(key, sharedPreferences.getBoolean(key, true));
+            app.settings().setUseDefaultSMS(sharedPreferences.getBoolean(key, true));
         }
-        editor.commit();
+
+        updatePrefs();
     }
 }
